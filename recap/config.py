358922fab2eca8cc0e24b6adb652ba6a45d62cc0
Geoffrey Smalling <cedralpass+git@gmail.com>
2024-04-22 09:24:11 -0700

    installed psycopg2-binary and configured app for postgres

--- recap/config.py
+++ recap/config.py
@@ -5,7 +5,7 @@ env.read_env()
 
 class Config:
     RECAP_Log_Level=env.str("RECAP_LogLevel")
-    SQLALCHEMY_DATABASE_URI='sqlite:////Users/geoffreysmalling/development/instance/recap_sqla.sqlite'
+    
     RECAP_SECRET_KEY=env.str("SECRET_KEY")
     RECAP_REDIS_URL=env.str("RECAP_REDIS_URL")
     RECAP_RQ_QUEUE=env.str("RECAP_RQ_QUEUE")
@@ -18,4 +18,14 @@ class Config:
     MAIL_DEFUALT_FROM= env.str("MAIL_DEFUALT_FROM")
     TASK_SERVER_NAME= env.str("TASK_SERVER_NAME")
     RECAP_AI_API_URL= env.str("RECAP_AI_API_URL") #config for calling classification service
+
+    #configure postgres variables
+    POSTGRES_USER=env.str("RECAP_POSTGRES_USER")
+    POSTGRES_PASSWORD=env.str("RECAP_POSTGRES_PASSWORD")
+    POSTGRES_HOST=env.str("RECAP_POSTGRES_HOST")
+    POSTGRES_PORT=env.str("RECAP_POSTGRES_PORT")
+    POSTGRES_DB=env.str("RECAP_POSTGRES_DB")
+    #Sqllite DB
+    #SQLALCHEMY_DATABASE_URI='sqlite:////Users/geoffreysmalling/development/instance/recap_sqla.sqlite'
+    SQLALCHEMY_DATABASE_URI=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
     
