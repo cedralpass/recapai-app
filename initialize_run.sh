6bfd0323e88c0f4795624a2d180bb1b631946438
Geoffrey Smalling <cedralpass+git@gmail.com>
2026-04-25 11:07:00 -0700

    Working to fix local and hosted render dockerfile issues

--- initialize_run.sh
+++ initialize_run.sh
@@ -5,18 +5,13 @@ cd /app
 #TODO: initialize DB needs to be a task  will do manually for now
 #flask --app recap init-db
 
-#start redis server as daemon
-#redis-server --daemonize yes
+# start redis server as daemon for local all-in-one container
+redis-server --daemonize yes
 
-# launch webserver as daemon
-gunicorn -w 3 -b 0.0.0.0:8000 app --log-level debug --timeout 90 --daemon
+# startup workers using monitoring script (same behavior pattern as Render)
+export RQ_QUEUE_NAME="RECAP2-Classify"
+export NUM_WORKERS=3
+bash /app/worker_monitor.sh &
 
-
-# startup workers
-#rq worker-pool --url $RECAP_REDIS_URL RECAP2-Classify -n 3
-
-# startup workers - using individual workers instead of worker-pool
-# worker-pool has a bug with --job-class parameter in rq 1.x versions
-rq worker --url $RECAP_REDIS_URL RECAP2-Classify &
-rq worker --url $RECAP_REDIS_URL RECAP2-Classify &
-rq worker --url $RECAP_REDIS_URL RECAP2-Classify &
+# launch webserver in foreground so container stays alive
+gunicorn -w 3 -b 0.0.0.0:8000 app --log-level debug --timeout 90
